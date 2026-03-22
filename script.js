8ad0babfa5bd8e895208c93341df9ed1065e3fbc
Prasham Kumbhare <prashamkumbhare2@gmail.com>
2026-03-22 22:21:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Conv_M_to_KM () {
+
+   let Value1 = document.getElementById("M_KM").value ;
+
+   let Final_Conv1 = Value1/1000 ;
+
+   document.getElementById("output1").innerHTML = Final_Conv1 + " " + "KM" ;
+
+}
