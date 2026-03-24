677f2d691bbe33afe277e6a3158c5c4ad443f2a4
Prasham Kumbhare <prashamkumbhare2@gmail.com>
2026-03-24 22:21:31 +0530

    Update script.js
    Functions Added.

--- script.js
+++ script.js
@@ -15,5 +15,46 @@ function Conv_KM_TO_M () {
    let Final_Conv2 = Value2 * 1000 ;
 
    document.getElementById("output2").innerHTML = Final_Conv2 + " " + "M" ;
-   
+
 }
+
+function Conv_CM_TO_M () {
+
+   let Value3 = document.getElementById("CM_M").value ;
+
+   let Final_Conv3 = Value3 / 100 ;
+
+   document.getElementById("output3").innerHTML = Final_Conv3 + " " + "M" ;
+
+}
+
+function Conv_M_TO_CM () {
+
+   let Value4 = document.getElementById("M_CM").value ;
+
+   let Final_Conv4 = Value4 * 100 ;
+
+   document.getElementById("output4").innerHTML = Final_Conv4 + " " + "CM" ;
+
+}
+
+function Conv_CM_TO_KM () {
+
+   let Value5 = document.getElementById("CM_KM").value ;
+
+   let Final_Conv5 = Value5 / 10000 ;
+
+   document.getElementById("output5").innerHTML = Final_Conv5 + " " + "KM" ;
+
+}
+
+function Conv_KM_TO_CM () {
+
+   let Value6 = document.getElementById("KM_CM").value ;
+
+   let Final_Conv6 = Value6 * 10000 ;
+
+   document.getElementById("output6").innerHTML = Final_Conv6 + " " + "CM" ;
+
+}
+
